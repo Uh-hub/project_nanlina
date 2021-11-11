ef59fd890ad0f1e072f4848510d5c6b0aab489ef
MinseoPark <kjms0820@gmail.com>
2021-11-12 00:57:21 +0900

    Add PM Info 1

---
+++ app/src/main/java/com/example/project_nanlina/PM.java
@@ -0,0 +1,37 @@
+package com.example.project_nanlina;
+
+public class PM {
+    String name;
+    String address;
+    String number;
+
+    public PM(String name, String address, String number) {
+        this.name = name;
+        this.address = address;
+        this.number = number;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public void setNumber(String number) {
+        this.number = number;
+    }
+}
