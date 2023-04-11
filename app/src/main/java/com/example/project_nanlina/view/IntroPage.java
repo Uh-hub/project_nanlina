bc4c6b4e52852c0b8a72e7ff1a68301d4175a054
M1nseoPark <kjms0820@gmail.com>
2023-04-11 23:55:40 +0900

    intro page

--- app/src/main/java/com/example/project_nanlina/view/IntroPage.java
+++ app/src/main/java/com/example/project_nanlina/view/IntroPage.java
@@ -1,9 +1,12 @@
 package com.example.project_nanlina.view;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.example.project_nanlina.R;
+import com.example.project_nanlina.controller.MainActivity;
 
 public class IntroPage extends Activity {
 
@@ -11,5 +14,19 @@ public class IntroPage extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.intro_page);
+        Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 2000);
+    }
+
+    protected void onPause() {
+        super.onPause();
+        finish();
     }
 }
